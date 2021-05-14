cfa2f5f1d65a60431deb18235cf6e270a5d256e7
kylewwelsh <kyle@kdmdental.com>
2021-05-14 00:24:32 -0600

    test for next section started, part one of javascript started

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,21 @@
-function noScroll() {
-  window.scrollTo(0, 0);
-}
+const optionOne = document.querySelector('.whatOption')
+const optionTwo = document.querySelector('.howOption')
+const optionThree = document.querySelector('.calculateOption')
+const what = document.querySelector('.what')
+const reload = document.querySelector('button')
 
-// add listener to disable scroll
-window.addEventListener('scroll', noScroll);
+reload.addEventListener (
+  'click', () => {
+    window.location.reload()
+  }
+)
 
-function HideScrollbar() {
-  var style = document.createElement("style");
-  style.innerHTML = `body::-webkit-scrollbar {display: none;}`;
-  document.head.appendChild(style);
-}
+optionOne.addEventListener (
+  'click', () => {
+    optionOne.classList.add('hidden')
+    optionTwo.classList.add('hidden')
+    optionThree.classList.add('hidden')
+    what.classList.remove('what')
+    what.classList.add('display')
+  }
+)
