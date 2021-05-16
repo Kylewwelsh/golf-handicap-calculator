0749fea49ab64a075cf9dfd25f09dc9408174624
kylewwelsh <kyle@kdmdental.com>
2021-05-16 11:02:45 -0600

    missed commits added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,25 @@ const optionOne = document.querySelector('.whatOption')
 const optionTwo = document.querySelector('.howOption')
 const optionThree = document.querySelector('.calculateOption')
 const what = document.querySelector('.what')
-const reload = document.querySelector('button')
+const how = document.querySelector('.how')
+const calculate = document.querySelector('.calculate')
+const reloadOne = document.querySelector('.reloadOne')
+const reloadTwo = document.querySelector('.reloadTwo')
+const reloadThree = document.querySelector('.reloadThree')
 
-reload.addEventListener (
+reloadOne.addEventListener (
+  'click', () => {
+    window.location.reload()
+  }
+)
+
+reloadTwo.addEventListener (
+  'click', () => {
+    window.location.reload()
+  }
+)
+
+reloadThree.addEventListener (
   'click', () => {
     window.location.reload()
   }
@@ -16,6 +32,26 @@ optionOne.addEventListener (
     optionTwo.classList.add('hidden')
     optionThree.classList.add('hidden')
     what.classList.remove('what')
-    what.classList.add('display')
+    what.classList.add('display', 'content')
+  }
+)
+
+optionTwo.addEventListener (
+  'click', () => {
+    optionOne.classList.add('hidden')
+    optionTwo.classList.add('hidden')
+    optionThree.classList.add('hidden')
+    how.classList.remove('how')
+    how.classList.add('display', 'content')
+  }
+)
+
+optionThree.addEventListener (
+  'click', () => {
+    optionOne.classList.add('hidden')
+    optionTwo.classList.add('hidden')
+    optionThree.classList.add('hidden')
+    calculate.classList.remove('calculate')
+    calculate.classList.add('display', 'content')
   }
 )
